0389388d908eae9db01e4152ef7543dc026d276d
Calum Imrie <s1120916@jaguar.inf.ed.ac.uk>
2014-01-20 17:36:28 +0000

    Changed it so its now using DifferentialPilot, this should ensure that the wheel motors start and stop at the same time

--- Milestone 1/src/Moving.java
+++ Milestone 1/src/Moving.java
@@ -1,60 +1,77 @@
 
-import lejos.nxt.ADSensorPort;
-import lejos.nxt.Button;
-import lejos.nxt.LightSensor;
 import lejos.nxt.*;
+import lejos.robotics.navigation.Move;
+import lejos.robotics.navigation.DifferentialPilot;
 
 public class Moving {
     /**
      * @param args
      */
 		
-	static LightSensor lightSensor = new LightSensor((ADSensorPort) SensorPort.S1);
+	static LightSensor lightSensor1 = new LightSensor((ADSensorPort) SensorPort.S1);
+	static LightSensor lightSensor2 = new LightSensor((ADSensorPort) SensorPort.S4);
 	static TouchSensor touchSensor = new TouchSensor((ADSensorPort) SensorPort.S2);
-    
+	static DifferentialPilot pilot = new DifferentialPilot(2.2f, 5.125f, Motor.A, Motor.B, false);	
+	
 	public static void main(String[] args) {
         
+		pilot.setTravelSpeed(7);
+
+		
 		while (true) {
+		    int x = lightSensor1.readValue();
+			int y = lightSensor2.readValue();
+			int z;
+			int distance;
+			
+//			Motor.A.setSpeed(550);  
+//			Motor.B.setSpeed(550);
 		
-			int x = lightSensor.readValue();
-			System.out.println(x);  
-			Motor.A.setSpeed(700);  
-			Motor.B.setSpeed(700);
-		
-			if (x > 40) {
+			if (x > 40 || y > 40) {
+			    pilot.setRotateSpeed(50);
+				while (x > 40 || y > 40) {
+					x = lightSensor1.readValue();
+					y = lightSensor2.readValue();
+//					Motor.A.setSpeed(200);
+//					Motor.B.setSpeed(200);
+//					
+//					Motor.A.forward();
+//				    Motor.B.backward();
+					pilot.rotateLeft();
+					
+				}
+			}
+			pilot.forward();
+		}
+               
+    }
+	
+	public void returnBack(int distance, int x, int y, int z) {
+		while (true) {
+			x = lightSensor1.readValue();
+			y = lightSensor2.readValue();
+			
+			if (x > 40 || y > 40) {
 
 				Motor.A.backward();
 				Motor.B.backward();
-				
-				while (x > 40) {
-					x = lightSensor.readValue();
-					Motor.A.setSpeed(100);
-					Motor.B.setSpeed(100);
+			
+				while (x > 40 || y > 40) {
+					x = lightSensor1.readValue();
+					y = lightSensor2.readValue();
+					Motor.A.setSpeed(200);
+					Motor.B.setSpeed(200);
 					
 					Motor.A.forward();
 				    Motor.B.backward();
 					
 				}
-				
+				z = Motor.B.getLimitAngle();
+				System.out.println(z);
 				Motor.A.stop();
 				Motor.B.stop();
-//				
-//				
-//				Motor.A.rotate(90);
-//				Motor.B.rotate(-90);
-//				while(Motor.A.isMoving() && Motor.B.isMoving()) { 
-//						Thread.yield();				
-//				}
-				
-//				Button.waitForAnyPress();
+
 			}
-			
-			Motor.A.forward();
-			Motor.B.forward();
-			
-//			Button.waitForAnyPress();
-			
 		}
-               
-    }
+	}	
 }
