5a475faf00e9630675a18af6f80b5c9341bda878
Calum Imrie <s1120916@audi.inf.ed.ac.uk>
2014-01-21 17:50:22 +0000

    milestone 1 working in most cases, uses differential pilot, stops once it has done some arbitrary distance once it reaches the whiteline

--- Milestone 1/src/Moving.java
+++ Milestone 1/src/Moving.java
@@ -1,16 +1,12 @@
 
 import lejos.nxt.*;
-import lejos.robotics.navigation.Move;
+//import lejos.robotics.navigation.Move;
 import lejos.robotics.navigation.DifferentialPilot;
 
 public class Moving {
-    /**
-     * @param args
-     */
 		
 	static LightSensor lightSensor1 = new LightSensor((ADSensorPort) SensorPort.S1);
 	static LightSensor lightSensor2 = new LightSensor((ADSensorPort) SensorPort.S4);
-	static TouchSensor touchSensor = new TouchSensor((ADSensorPort) SensorPort.S2);
 	static DifferentialPilot pilot = new DifferentialPilot(2.2f, 5.125f, Motor.A, Motor.B, false);	
 	
 	public static void main(String[] args) {
@@ -19,55 +15,73 @@ public class Moving {
 
 		boolean reached = false;
 		
-		while (!reached) {
-		    int x = lightSensor1.readValue();
-			int y = lightSensor2.readValue();
-			float z = 0;
-			float distance = 0;
+		int x = lightSensor1.readValue();
+		int y = lightSensor2.readValue();
+		float distance = 0;
 		
+		while (!reached) {
+		    x = lightSensor1.readValue();
+			y = lightSensor2.readValue();
+
 			if (x > 40 || y > 40) {
-			    pilot.setRotateSpeed(50);
+			    pilot.setRotateSpeed(40);
+
+			    while (x > 40 || y > 40) {
+					x = lightSensor1.readValue();
+					y = lightSensor2.readValue();
+     				pilot.rotateLeft();	
+				}
+				reached = true;
+			} else { 
+				pilot.forward();
+			}
+		}          
+		
+		while (distance < 30) {
+
+		    x = lightSensor1.readValue();
+			y = lightSensor2.readValue();
+			
+			if (x > 40 || y > 40) {
+			    pilot.setRotateSpeed(40);
 			    distance += pilot.getMovementIncrement();
 				while (x > 40 || y > 40) {
 					x = lightSensor1.readValue();
 					y = lightSensor2.readValue();
      				pilot.rotateLeft();	
-     				z += pilot.getAngleIncrement();
 				}
-			    //z = pilot.getAngleIncrement();
-			    System.out.println(z);
-			    System.out.println(distance);
-				returnBack(x, y, z, distance);
-				reached = true;
-			} else{ 
+			} else { 
 				pilot.forward();
 				distance += pilot.getMovementIncrement();
 			}
-		}              
+		
+		}
+		
     }
 	
-	public static void returnBack(int x, int y, float z, float distance) {
-		
-		pilot.setTravelSpeed(7);
-		float angleTurned =0;
-		
-		while (angleTurned < 180) {
-		    x = lightSensor1.readValue();
-			y = lightSensor2.readValue();
-			
-			if (x > 40 || y > 40) {
-			    pilot.setRotateSpeed(50);
-				while (x > 40 || y > 40) {
-					x = lightSensor1.readValue();
-					y = lightSensor2.readValue();
-     				pilot.rotateLeft();
-     				angleTurned += pilot.getAngleIncrement();
-				}
-			}
-			pilot.forward();
-		}  
-		
-		pilot.rotate(-z);
-		pilot.travel(20, false);
-	}	
+	
+//	public static void returnBack(int x, int y, float z, float distance) {
+//		
+//		pilot.setTravelSpeed(7);
+//		float angleTurned =0;
+//		
+//		while (angleTurned < 180) {
+//		    x = lightSensor1.readValue();
+//			y = lightSensor2.readValue();
+//			
+//			if (x > 40 || y > 40) {
+//			    pilot.setRotateSpeed(50);
+//				while (x > 40 || y > 40) {
+//					x = lightSensor1.readValue();
+//					y = lightSensor2.readValue();
+//     				pilot.rotateLeft();
+//     				angleTurned += pilot.getAngleIncrement();
+//				}
+//			}
+//			pilot.forward();
+//		}  
+//		
+//		pilot.rotate(-z);
+//		pilot.travel(20, false);
+//	}	
 }
