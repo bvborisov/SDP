7c5a40c9f8ce3343e75b157112f4d31c33a3e6d7
Anders-Petter Ljungquist <anderspetter.ljungquist@gmail.com>
2014-01-24 23:05:56 +0000

    Tried adding some comments.

--- Milestone 1/src/EdgeFollower.java
+++ Milestone 1/src/EdgeFollower.java
@@ -1,3 +1,9 @@
+/*
+ * I'm a pretty lousy commenter but I tried adding some comments. 
+ * I'm sure many comments are redundant and areas that should be commented are not.
+ * Feel free to use the comment functionality of github or in some other way 
+ * point out and ask about anything that is unclear. 
+ */
 import lejos.nxt.Button;
 import lejos.nxt.LightSensor;
 import lejos.nxt.Motor;
@@ -8,20 +14,30 @@ import lejos.robotics.navigation.Pose;
 
 public class EdgeFollower {
 
+	/*
+	 * Setting up the robot
+	 */
 	static final LightSensor leftLight = new LightSensor(SensorPort.S4);;
 	static final LightSensor rightLight = new LightSensor(SensorPort.S1);;
 	static final DifferentialPilot pilot = new DifferentialPilot(5.6, 9.7, Motor.B, Motor.A, true);
 	static final OdometryPoseProvider opp = new OdometryPoseProvider(pilot);
 	
+	/*
+	 * Flags to keep track of what conditions have been satisfied
+	 */
 	private static boolean foundEdge = false;
 	private static boolean departed = false;
 	private static boolean returned = false;
 	private static boolean stopped = false;
 	
+	/*
+	 * Fields to keep track of displacement
+	 */
 	private static double currentDisplacement;
 	private static double minDisplacement;
 
 	public static void main(String[] aArg) throws Exception {
+		//Allows the pose to be updated also during a movement and not just after
 		pilot.addMoveListener(opp);
 		
 		System.out.println("EdgeFollower");
@@ -29,19 +45,29 @@ public class EdgeFollower {
 		Button.waitForAnyPress();
 		
 		while (!stop()) {
+			/*
+			 * Until the stop condition is satisfied the robot will try to follow the
+			 * edge as closely as possible but never be on it. That is to say it will
+			 * try to always have both sensors over a green area. 
+			 */
 			if (seesOnlyGreen()) {
 				pilot.setTravelSpeed(10);
 				pilot.forward();
 				while (seesOnlyGreen() && !stop()) {
-					//Keep moving forward
+					/*
+					 * Robot keeps moving forward until it either sees white or
+					 * satisfies the stop condition
+					 */
 				}
 				pilot.stop();
 			}
-			else if (seesWhite()) {
+			else if (seesSomeWhite()) {
 				pilot.setTravelSpeed(5);
 				pilot.rotateLeft();
-				while (seesWhite()) {
-					//Keep rotating
+				while (seesSomeWhite()) {
+					/*
+					 * The robot keeps rotating left until is only sees green
+					 */
 				}
 				pilot.stop();
 			}
@@ -50,6 +76,10 @@ public class EdgeFollower {
 		Button.waitForAnyPress();
 	}
 	
+	/*
+	 * The robot should stop once it is in the return zone and 
+	 * the displacement from the origin starts increasing.
+	 */
 	private static boolean stop() {
 		if (stopped) return true;
 		updateDisplacements();
@@ -60,6 +90,10 @@ public class EdgeFollower {
 		}else return false;
 	}
 	
+	/*
+	 * The robot is in the return zone if it has left the departure zone and
+	 * its displacement from the origin is less than some threshold.
+	 */
 	private static boolean hasReturned() {
 		if (returned) return true;
 		if (hasDeparted() && currentDisplacement < 10) {
@@ -69,6 +103,10 @@ public class EdgeFollower {
 		}else return false;
 	}
 	
+	/*
+	 * The robot has left the departure zone if it has found the edge and
+	 * its displacement from the origin is more than some threshold.
+	 */
 	private static boolean hasDeparted() {
 		if (departed) return true;
 		if (hasFoundEdge() && currentDisplacement > 20) {
@@ -78,9 +116,17 @@ public class EdgeFollower {
 		} else return false;
 	}
 	
+	/*
+	 * The robot had found the edge if it at any point has encountered the color white.
+	 */
 	private static boolean hasFoundEdge() {
 		if (foundEdge) return true;
-		if (seesWhite()) {
+		if (seesSomeWhite()) {
+			/*
+			 * When the robot first finds the edge it sets that position as the 
+			 * origin in its positioning system. It also updates displacement values
+			 * as these should now be 0;
+			 */
 			opp.setPose(new Pose());
 			updateDisplacements();
 			
@@ -90,6 +136,10 @@ public class EdgeFollower {
 		}else return false;
 	}
 	
+	/*
+	 * The displacement is calculated as the euclidean distance from the origin to
+	 * the current position of the robot.
+	 */
 	private static void updateDisplacements() {
 		currentDisplacement = Math.sqrt(
 				Math.pow(opp.getPose().getX(), 2) + 
@@ -112,7 +162,7 @@ public class EdgeFollower {
 		return seesWhite(leftLight) != seesWhite(rightLight);
 	}
 
-	private static boolean seesWhite() {
+	private static boolean seesSomeWhite() {
 		return seesWhite(leftLight) || seesWhite(rightLight);
 	}
 
