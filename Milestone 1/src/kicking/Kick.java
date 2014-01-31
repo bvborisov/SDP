3429523484e6edf1afa3330d062e299fa65df902
ccimrie <imrieimrie@gmail.com>
2014-01-31 12:36:28 +0000

    Inlcudes the world and geometry packages from sdp4 last year.

---
+++ Milestone 1/src/kicking/Kick.java
@@ -0,0 +1,11 @@
+package kicking;
+
+import lejos.nxt.Motor;
+
+public class Kick {
+
+	public static void main (String[] args) {
+		
+	}
+	
+}
---
+++ Milestone 1/src/world/state/Robot.java
@@ -0,0 +1,25 @@
+package world.state;
+
+public class Robot extends Entity {
+	
+	public double x;
+	public double y;
+	/** In radians*/
+	public double bearing;
+	public RobotType type;
+	
+	public Robot(RobotType type) {
+		this.type = type;
+	}
+	
+
+	public String name() {
+		return "Robot (" + type.toString() + ")";
+	}
+	
+	public String position() {
+		return String.format("%s: %s bearing %f",
+				this.type, this.getPosition(), this.bearing);
+	}
+	
+}
