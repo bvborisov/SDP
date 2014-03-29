c51aa17e88c24595e4d1d3bae664ba278a24a12f
RAbadzhiev1 <rabadzhiev@yahoo.com>
2014-03-29 21:16:21 +0000

    Test for the Attacker and updated Attacker code.

--- Milestone 1/src/robot/DefenderBrick.java
+++ Milestone 1/src/robot/DefenderBrick.java
@@ -13,25 +13,8 @@ public class DefenderBrick {
 	//Setting up the robot
 	//static final OmniPilot pilot = new OmniPilot(6.5f, 4.7f, Motor.A, Motor.C, Motor.B, true, true);
 	private final static DifferentialPilot pilot = new DifferentialPilot(13.4, 16.5, Motor.B, Motor.A);
-	private static double rotateSpeed = 5;
-    private static double travelSpeed = 20;
-	private static InputStream dis;
-
-    // Commands
- 	private final static int NOTHING = 0;
- 	private final static int FORWARDS = 1;
- 	private final static int BACKWARDS = 2;
- 	private final static int STOP = 3;
- //	private final static int KICK = 4;
- 	private final static int SPEED = 5;
- 	private final static int ROTATELEFT = 6;
- 	private final static int ROTATERIGHT = 7;
- 	private final static int QUIT = 9;
  	
  	public static void main(String[] args) throws Exception {
- 	//Set initial command and initialise options
-	int command = NOTHING;
-	int option1, option2, option3;
 			
 	while (true) {
  
@@ -56,27 +39,6 @@ public class DefenderBrick {
 	}
 
 		
-			
-		
-				//pilot.backward();
-	
-//				pilot.stop();
-		
-				
-//			case KICK:
-//				
-//				break;
-	  			
-		
-
-	
-			
-	
-	//			pilot.rotate(90);
-
-			
-
-		//		pilot.rotate(-90);
 
 
 	}
